040a13a79d344cab26bb50cb860c0961ae3944f4
NelonArrive <nelon.arrive@gmail.com>
2025-05-30 01:07:13 +0300

    🎉 added Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,29 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { logger } from './common/middlewares/logger.middleware'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.enableCors()
 	app.setGlobalPrefix('api')
-	app.use(logger)
 	app.useGlobalPipes(new ValidationPipe())
+
+	const config = new DocumentBuilder()
+		.setTitle('Nest Course API')
+		.setDescription('API documentation fot Nest course')
+		.setVersion('1.0.0')
+		.setContact('Nelon Arrive', 'https://tlelegramm.com', 'nelon@arrive.com')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('/docs', app, document)
+
+	app.use(logger)
 
 	await app.listen(4200)
 }
 bootstrap()
+
+// 4:54:00
