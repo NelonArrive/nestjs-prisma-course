1ac360a54b98e8fb5e7a7dbd2a736e402ab1e28c
NelonArrive <nelon.arrive@gmail.com>
2025-05-30 19:20:09 +0300

    🎉 added Swagger APIs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { logger } from './common/middlewares/logger.middleware'
-import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
